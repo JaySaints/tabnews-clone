2669a62f8ec749b03a109aa5f2dbae08d5790cab
JaySaints <pablojs2008@hotmail.com>
2024-10-18 19:38:21 -0300

    Edit endpoint `/status` update database infos

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0]);
-  res.status(200).json({ msg: "Hello JaySaints!" });
+  const updatedAt = new Date().toISOString();
+  const queryOpenedConnections =
+    "SELECT count(datid) as opened_connections FROM pg_stat_activity;";
+  const resultOpenedConnections = await database.query(queryOpenedConnections);
+  const openedConnections = parseInt(
+    resultOpenedConnections.rows[0].opened_connections
+  );
+
+  const queryMaxConnections = "SHOW max_connections;";
+  const resultMaxConnections = await database.query(queryMaxConnections);
+  const maxConnections = parseInt(resultMaxConnections.rows[0].max_connections);
+
+  const queryDatabaseVersion = "SELECT version();";
+  const resultDatabaseVersion = await database.query(queryDatabaseVersion);
+  const splitResult = resultDatabaseVersion.rows[0].version.split(" ");
+  const databaseVersion = splitResult[1];
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      status: "",
+      max_connections: maxConnections,
+      opened_connections: openedConnections,
+      version: databaseVersion,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status Check properties", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  // console.log(responseBody);
+
   expect(response.status).toBe(200);
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 });
